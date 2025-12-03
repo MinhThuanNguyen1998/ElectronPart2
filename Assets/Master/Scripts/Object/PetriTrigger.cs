686f5364135ad037ee13f4de6d0481a158e98e32
thuannm <thuannm.ot@gmail.com>
2025-12-03 17:01:58 +0700

    - Update : logic set parent when entering petri trigger

--- Assets/Master/Scripts/Object/PetriTrigger.cs
+++ Assets/Master/Scripts/Object/PetriTrigger.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 public class PetriTrigger : MonoBehaviour
 {
     [SerializeField] StepSolid m_StepSolid;
+    [SerializeField] GameObject m_ParentPetri;
+    [SerializeField] GameObject m_EmptyParent;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Solid"))
@@ -10,6 +12,8 @@ public class PetriTrigger : MonoBehaviour
             m_StepSolid = other.GetComponent<StepSolid>();
             m_StepSolid.GoToNextStep();
             MagnifyingManager.Instance.ActiveMagnifyingObject(true);
+            other.transform.SetParent(m_ParentPetri.transform);
+
         }
     }
     private void OnTriggerExit(Collider other)
@@ -19,6 +23,7 @@ public class PetriTrigger : MonoBehaviour
             m_StepSolid = other.GetComponent<StepSolid>();
             m_StepSolid.GoToPrevStep();
             MagnifyingManager.Instance.ActiveMagnifyingObject(false);
+            other.transform.SetParent(m_EmptyParent.transform);
         }
     }
     
