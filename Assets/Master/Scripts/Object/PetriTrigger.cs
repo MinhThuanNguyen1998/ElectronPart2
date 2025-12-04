9f15bf70c4f2fd880e733ab37c6a5e34138aeee1
thuannm <thuannm.ot@gmail.com>
2025-12-04 16:00:28 +0700

    - Update : Preventing solids from falling out of the scene

--- Assets/Master/Scripts/Object/PetriTrigger.cs
+++ Assets/Master/Scripts/Object/PetriTrigger.cs
@@ -5,14 +5,23 @@ public class PetriTrigger : MonoBehaviour
     [SerializeField] StepSolid m_StepSolid;
     [SerializeField] GameObject m_ParentPetri;
     [SerializeField] GameObject m_EmptyParent;
+    [SerializeField] GameObject m_CyclinderPetri;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Solid"))
         {
+            
             m_StepSolid = other.GetComponent<StepSolid>();
-            m_StepSolid.GoToNextStep();
-            MagnifyingManager.Instance.ActiveMagnifyingObject(true);
-            other.transform.SetParent(m_ParentPetri.transform);
+            float otherY = other.transform.position.y;
+            float thisY = transform.position.y;
+            if (otherY < thisY) m_CyclinderPetri.SetActive(false);
+            else 
+            {
+                m_CyclinderPetri.SetActive(true);
+                m_StepSolid.GoToNextStep();
+                MagnifyingManager.Instance.ActiveMagnifyingObject(true);
+                other.transform.SetParent(m_ParentPetri.transform);
+            }
         }
     }
     private void OnTriggerExit(Collider other)
