d3fedd9f39ad860300017e308b3b109ba7ad4f92
thuannm <thuannm.ot@gmail.com>
2025-12-05 08:54:31 +0700

    - Update : bottom collider for petri

--- Assets/Master/Scripts/Object/PetriTrigger.cs
+++ Assets/Master/Scripts/Object/PetriTrigger.cs
@@ -5,23 +5,15 @@ public class PetriTrigger : MonoBehaviour
     [SerializeField] StepSolid m_StepSolid;
     [SerializeField] GameObject m_ParentPetri;
     [SerializeField] GameObject m_EmptyParent;
-    [SerializeField] GameObject m_CyclinderPetri;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Solid"))
         {
-            
             m_StepSolid = other.GetComponent<StepSolid>();
-            float otherY = other.transform.position.y;
-            float thisY = transform.position.y;
-            if (otherY < thisY) m_CyclinderPetri.SetActive(false);
-            else 
-            {
-                m_CyclinderPetri.SetActive(true);
-                m_StepSolid.GoToNextStep();
-                MagnifyingManager.Instance.ActiveMagnifyingObject(true);
-                other.transform.SetParent(m_ParentPetri.transform);
-            }
+            m_StepSolid.GoToNextStep();
+            MagnifyingManager.Instance.ActiveMagnifyingObject(true);
+            other.transform.SetParent(m_ParentPetri.transform);
         }
     }
     private void OnTriggerExit(Collider other)
